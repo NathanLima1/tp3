8c1cfc7154f66cac1ec303d528e2f2c09c30a7aa
Leo6485 <lo570354@gmail.com>
2025-06-03 18:09:38 -0300

    .

--- parte_2/shift_and.c
+++ parte_2/shift_and.c
@@ -6,21 +6,21 @@ int main() {
     int k = 2;
     int *states = (int *)malloc(k * sizeof(int));
 
-    char *text = "texto para testar o teste\0";
+    char *text = "texto para testar o tes dte\0";
     char *padrao = "teste\0";
 
     int n = strlen(text);
     int m = strlen(padrao);
 
-    int *masks = (int*)malloc(26 * sizeof(int));
+    int *masks = (int*)calloc(26, sizeof(int));
 
     int j = 0;
-    for (int i = 0; i < 26; i++) {
-        masks[i] = 0;
+    for (int i = 0; i < m; i++) {
+        j = padrao[i] - 97;
+        if (masks[j] != 0) continue; // Já processou essa letra
         for (int k = 0; k < m; k++) {
-            masks[i] = (masks[i] << 1) + (i + 97 == padrao[k]); // Desloca 1 para a esquerda e adiciona 1 se for igual
+            masks[j] = (masks[j] << 1) + (j + 97 == padrao[k]); // Desloca 1 para a esquerda e adiciona 1 se for igual
         }
-        j++;
     }
 
     for (int i = 0; i < 26; i++) {
