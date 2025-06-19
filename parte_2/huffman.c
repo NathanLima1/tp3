0b74dd60bb01f59d47937c51caaecda13fc42fb9
Leo6485 <lo570354@gmail.com>
2025-06-19 19:42:24 -0300

    .

---
+++ parte_2/huffman.c
@@ -0,0 +1,18 @@
+#include "huffman.h"
+#include "./utils/hash-table.h"
+
+void get_word_frequency(Table *t, char *text) {
+
+}
+
+void encode() {
+
+}
+
+void decode() {
+
+}
+
+int main() {
+    return 0;
+}
