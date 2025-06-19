34b732fd25c30678ff15c3ac804051a77b95a21b
Leo6485 <lo570354@gmail.com>
2025-06-19 16:52:00 -0300

    hash-table.c adicionado

---
+++ parte_2/utils/hash-table.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct tableItem{
+    char *key;
+    int value;
+    struct tableItem *next;
+
+} tableItem;
+
+typedef struct {
+    tableItem **data;
+    int size;
+} Table;
+
+unsigned int hash(char *s) {
+    int len = strlen(s);
+    unsigned int hash = 1;
+    for (int i = 0; i < len; i++) {
+        hash = hash * 33 + s[i];
+    }
+
+    return hash;
+}
+
+Table *initTable(int size) {
+    Table *t = malloc(sizeof(Table));
+    t->data = calloc(size, sizeof(tableItem*));
+    t->size = size;
+    return t;
+}
+int insert(Table *t, char *key) {
+    int index = hash(key) % t->size;
+    tableItem *crr_item = t->data[index];
+
+    while (crr_item != NULL) {
+        if (strcmp(crr_item->key, key) == 0) {
+            crr_item->value++;
+            return 0;
+        }
+        crr_item = crr_item->next;
+    }
+
+    tableItem *item = malloc(sizeof(tableItem));
+    item->key = strdup(key);
+    item->value = 1;
+    item->next = t->data[index];
+    t->data[index] = item;
+}
+
+void printTableFormatted(Table *t) {
+    printf("+--------+----------------------+--------+\n");
+    printf("| Index  | Key                  | Value  |\n");
+    printf("+--------+----------------------+--------+\n");
+
+    for (int i = 0; i < t->size; i++) {
+        tableItem *curr = t->data[i];
+        while (curr) {
+            printf("| %-6d | %-20s | %-6d |\n", i, curr->key, curr->value);
+            curr = curr->next;
+        }
+    }
+
+    printf("+--------+----------------------+--------+\n");
+}
+
+int main() {
+    Table *t = initTable(5);
+    insert(t, "pato\0");
+    insert(t, "pato\0");
+    insert(t, "pato\0");
+    insert(t, "pato\0");
+    insert(t, "pato\0");
+    insert(t, "topa\0");
+    insert(t, "a\0");
+    printTableFormatted(t);
+    return 0;
+}
